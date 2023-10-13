990ca4d6a442181d5586226c124fcbb7fba1fe13
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-10-13 20:59:29 +0100

    qr code add

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./home.css";
 
 const Home = () => {
+  const [qrCodeUrl, setQRCodeUrl] = useState("");
+
+  const getQR = async () => {
+    const response = await fetch("https://qr-generator-0dsd.onrender.com/qr");
+    const url = await response.text();
+    setQRCodeUrl(url);
+  };
+
+  const onGenerateQR = () => {
+    getQR();
+  };
+
   return (
     <div className="home">
-      <button className="qr">Generate QR code</button>
+      <button className="qr" onClick={onGenerateQR}>
+        Generate QR code
+      </button>
+      {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}
     </div>
   );
 };
